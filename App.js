f1b91d70185f66064cc8ec4d61ffcddbe8a8e2f3
Darc99 <lennyyhugoh@gmail.com>
2022-03-24 04:59:27 +0100

    added history list display and clear function

--- App.js
+++ App.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import { Focus } from './src/features/focus/Focus';
+import { FocusHistory } from './src/features/focus/FocusHistory';
 import { Timer } from './src/features/timer/Timer';
 import { colors } from './src/utils/colors';
 import { spacing } from './src/utils/sizes';
 
 export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
-  const [focusHistory, setFocusHistory] = useState([])
+  const [focusHistory, setFocusHistory] = useState([]);
 
   const addFocusHistorySubject = (subject, status) => {
-    setFocusHistory([...focusHistory, {subject, status }])
+    setFocusHistory([...focusHistory, {subject, status }]);
   }
 
   const STATUES = {
     COMPLETE: 1,
     CANCELLED: 2
   }
+
+  const onClear = () => {
+    setFocusHistory([]);
+  }
+
 
   return (
     <View style={styles.container}>
@@ -34,7 +40,10 @@ export default function App() {
           }}
         />
       ) : (
-        <Focus addSubject={setFocusSubject} />
+        <>
+          <Focus addSubject={setFocusSubject} />
+          <FocusHistory focusHistory={focusHistory} onClear={onClear}/>
+        </>
       )}
     </View>
   );
